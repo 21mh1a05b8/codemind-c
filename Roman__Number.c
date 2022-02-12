a532c678873f98e1bb36aa77b74b0ba97a29a5a1
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-02-12 19:58:21 +0530

    #include <stdio.h> int main() {     int num, rem;     scanf("%d", &num);     while(num != 0)     {         if (num >= 1000)         {            printf("M");            num -= 1000;         }
            else if (num >= 900)   
            {
               printf("CM");
               num -= 900;
            }        
    
            else if (num >= 500)
            {           
               printf("D");
               num -= 500;
            }
    
            else if (num >= 400)   // 400 -  cd
            {
               printf("CD");
               num -= 400;
            }
    
            else if (num >= 100)   // 100 - c
            {
               printf("C");
               num -= 100;                       
            }
    
            else if (num >= 90)    // 90 - xc
            {
               printf("XC");
               num -= 90;                                              
            }
    
            else if (num >= 50)    // 50 - l
            {
               printf("L");
               num -= 50;                                                                     
            }
    
            else if (num >= 40)    // 40 - xl
            {
               printf("XL");
               num -= 40;
            }
    
            else if (num >= 10)    // 10 - x
            {
               printf("X");
               num -= 10;           
            }
    
            else if (num >= 9)     // 9 - ix
            {
               printf("IX");
               num -= 9;                         
            }
    
            else if (num >= 5)     // 5 - v
            {
               printf("V");
               num -= 5; 
            }
    
            else if (num >= 4)     // 4 - iv
            {
               printf("IV");
               num -= 4;                                                            
            }
    
            else if (num >= 1)     // 1 - i
            {
               printf("I");
               num -= 1;                                                                                   
            }
    
        }
        return 0;
    }

---
+++ Roman__Number.c
@@ -0,0 +1,88 @@
+#include <stdio.h> 
+int main() 
+{   
+    int num, rem;
+    scanf("%d", &num);
+    while(num != 0)
+    {
+        if (num >= 1000)     
+        {
+           printf("M");
+           num -= 1000;
+        }
+
+        else if (num >= 900)   
+        {
+           printf("CM");
+           num -= 900;
+        }        
+
+        else if (num >= 500)
+        {           
+           printf("D");
+           num -= 500;
+        }
+
+        else if (num >= 400)   // 400 -  cd
+        {
+           printf("CD");
+           num -= 400;
+        }
+
+        else if (num >= 100)   // 100 - c
+        {
+           printf("C");
+           num -= 100;                       
+        }
+
+        else if (num >= 90)    // 90 - xc
+        {
+           printf("XC");
+           num -= 90;                                              
+        }
+
+        else if (num >= 50)    // 50 - l
+        {
+           printf("L");
+           num -= 50;                                                                     
+        }
+
+        else if (num >= 40)    // 40 - xl
+        {
+           printf("XL");
+           num -= 40;
+        }
+
+        else if (num >= 10)    // 10 - x
+        {
+           printf("X");
+           num -= 10;           
+        }
+
+        else if (num >= 9)     // 9 - ix
+        {
+           printf("IX");
+           num -= 9;                         
+        }
+
+        else if (num >= 5)     // 5 - v
+        {
+           printf("V");
+           num -= 5; 
+        }
+
+        else if (num >= 4)     // 4 - iv
+        {
+           printf("IV");
+           num -= 4;                                                            
+        }
+
+        else if (num >= 1)     // 1 - i
+        {
+           printf("I");
+           num -= 1;                                                                                   
+        }
+
+    }
+    return 0;
+}    
