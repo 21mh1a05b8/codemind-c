48c552b85115751f797ac897a08d2f0059cce813
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-04-19 14:19:01 +0530

    #include<stdio.h>
    int main()  
    {  
        int rows,i,j,k;  
        scanf("%d",&rows);  
        for( i=rows;i>=1;i--)  
        {  
            for( j=1;j<=i-1;j++)  
            {  
                printf(" ");  
            }  
            for( k=1;k<=rows;k++)  
            {  
               if(i==1 || i==rows || k==1 || k==rows)  
                printf("*");  
                else  
                printf(" ");   
            }  
            printf("
    ");  
        }  
        return 0;  
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
-int main() 
-{
-   int i, j, rows;
-   scanf("%d", &rows);
-   for (i = rows; i >= 1; --i)
-   {
-      for (j = 1; j <= i; ++j) 
-      {
-         printf("%d", j);
-      }
-      printf("
-");
-   }
-   return 0;
+#include<stdio.h>
+  
+int main()  
+{  
+    int rows,i,j,k;  
+    scanf("%d",&rows);  
+    for( i=rows;i>=1;i--)  
+    {  
+        for( j=1;j<=i-1;j++)  
+        {  
+            printf(" ");  
+        }  
+        for( k=1;k<=rows;k++)  
+        {  
+           if(i==1 || i==rows || k==1 || k==rows)  
+            printf("*");  
+            else  
+            printf(" ");   
+        }  
+        printf("
+");  
+    }  
+    return 0;  
 }
