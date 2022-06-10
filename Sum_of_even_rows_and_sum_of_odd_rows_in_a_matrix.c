5e29c39a152130822515ec40cc25132fa42d9e2f
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-06-10 14:34:33 +0530

    #include<stdio.h> int main() {      int a[100][100],r,c,sum=0,i,j,sum1=0;      scanf("%d%d",&r,&c);      for(i=0;i<r;i++)      {         for(j=0;j<c;j++)         {              scanf("%d",&a[i][j]);              if (i%2==0)              {              sum=sum+a[i][j];              }              else              {                  sum1=sum1+a[i][j];              }         }      }      printf("%d %d",sum,sum1);      return 0; }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+     int a[100][100],r,c,sum=0,i,j,sum1=0;
+     scanf("%d%d",&r,&c);
+     for(i=0;i<r;i++)
+     {
+        for(j=0;j<c;j++)
+        {
+             scanf("%d",&a[i][j]);
+             if (i%2==0)
+             {
+             sum=sum+a[i][j];
+             }
+             else
+             {
+                 sum1=sum1+a[i][j];
+             }
+        }
+     }
+     printf("%d %d",sum,sum1);
+     return 0;
+}
