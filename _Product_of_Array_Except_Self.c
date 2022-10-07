c2a1074435dc3cddafff4813b569dcc12d7d8190
21mh1a05b8 <97426089+21mh1a05b8@users.noreply.github.com>
2022-10-07 21:45:26 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,k=1;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         k=1;         for(j=0;j<n;j++)         {             if (i!=j)             {                 k=k*arr[j];             }         }         printf("%d ",k);     } }

---
+++ _Product_of_Array_Except_Self.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,k=1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        k=1;
+        for(j=0;j<n;j++)
+        {
+            if (i!=j)
+            {
+                k=k*arr[j];
+            }
+        }
+        printf("%d ",k);
+    }
+}
